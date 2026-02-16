64cfe939ec429fbff26c846cf677b104fd3aeda1
subhankar <bnk.subhankar@gmail.com>
2026-02-16 15:36:03 +0530

    Changes in code

---
+++ Day-10/src/routes/auth.routes.js
@@ -0,0 +1,37 @@
+const express=require("express")
+const jwt=require("jsonwebtoken")
+const userModel=require("../models/user.model")
+const authRouter=express.Router()
+
+authRouter.post("/register",async(req,res)=>{
+    const {email,name,password}=req.body
+
+    const isUserAlreadyExist=await userModel.findOne({email})
+
+    if(isUserAlreadyExist)
+    {
+        return res.status(400).json({
+            message:"User Already Exist"
+        })
+    }
+    const user=await userModel.create({
+        name,email,password
+    })
+    
+    const token=jwt.sign(
+        {
+        id:user._id,
+        email:user.email
+        },
+        process.env.JWT_SECRET)
+
+        res.cookie("jwt_token",token)
+
+    res.status(201).json({
+        message:"User Registered Successfully",
+        user,
+        token
+    })
+})
+
+module.exports=authRouter
