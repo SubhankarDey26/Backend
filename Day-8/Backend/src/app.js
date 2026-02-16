8df289f779547b426ea4307751dc9e2b77af68d2
subhankar <bnk.subhankar@gmail.com>
2026-02-16 15:33:05 +0530

    jwt implementation

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const cors=require("cors")
 const noteModel=require("./models/note.models")
+const path=require("path")
 const app=express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("./public"))
 
 app.post("/api/notes",async(req,res)=>{
     const {title,description}=req.body
@@ -51,5 +53,10 @@ app.patch("/api/notes/:id",async(req,res)=>{
     })
 })
 
+
+// WildCard API
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
 
 module.exports=app
--- Day-8/Frontend/src/App.jsx
+++ Day-8/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const App = () => {
@@ -22,19 +22,58 @@ const App = () => {
     }
 ])
 
-  axios.get('http://localhost:3000/api/notes')
+  function fetchNotes(){
+      axios.get('http://localhost:3000/api/notes')
   .then((res)=>{
     setnote(res.data.note)
   })
+  }
+
+  useEffect(()=>{
+    fetchNotes()
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const {title,description}=e.target.elements
+    console.log(title.value,description.value)
+
+
+    axios.post("http://localhost:3000/api/notes",{
+      title:title.value,
+      description:description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+  }
+
+function handledeleteNotes(noteId){
+  axios.delete("http://localhost:3000/api/notes/"+noteId)
+  .then(res=>{
+    console.log(res.data)
+    fetchNotes()
+  })
+}
 
   return (
     <>
+  <form className='note-create-form' onSubmit={handleSubmit}>
+    <input name='title' type="text" placeholder='Enter Title' />
+    <input name='description' type="text" placeholder='Enter Descritpion' />
+    <button>Create Note</button>
+  </form>
+
     <div className='notes'>
       {
         note.map((note,id)=>{
           return <div className="note" key={id}>
         <h1>{note.title}</h1>
         <p>{note.description}</p>
+        <button onClick={()=>{
+          {handledeleteNotes(note._id)}
+        }}>Delete</button>
       </div>
         })
       }
