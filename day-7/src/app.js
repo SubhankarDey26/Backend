a773d96daf43c7015af7d6b8a2d230892e086f8c
subhankar <bnk.subhankar@gmail.com>
2026-04-20 20:01:03 +0530

    Updated day-7

--- day-7/src/app.js
+++ day-7/src/app.js
@@ -10,17 +10,16 @@ const notes=[]
 app.post("/notes",async(req,res)=>{
     const {title,description}=req.body
 
-   const note= await noteModel.create({
+   const notes= await noteModel.create({
         title,description
     })
 
 
     res.status(201).json({
         message:"Note Created SUcessfully",
-        note
+        notes
     })
 })
-
 
 app.get("/notes",async(req,res)=>{
     const notes =await noteModel.find()
@@ -31,7 +30,22 @@ app.get("/notes",async(req,res)=>{
     })
 })
 
+app.delete("/notes/:id",async(req,res)=>{
+    const id=req.params.id
+    await noteModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message:"Note Deleted Sucessfully"
+    })
+})
 
+app.patch("/notes/:id",async(req,res)=>{
+    const id=req.params.id
+    const {description}=req.body
+    await noteModel.findByIdAndUpdate(id,{description})
+    res.status(200).json({
+        message:"Note Updated Suceefully"
+    })
+})
 
 
 module.exports=app
