760675a95e054f05a83eb954d5411a4facedad18
subhankar <bnk.subhankar@gmail.com>
2026-05-07 12:18:53 +0530

    Learned About Login and Register

---
+++ day-8/src/controllers/auth.controller.js
@@ -0,0 +1,88 @@
+const authModel=require("../models/user.model")
+const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
+const userModel = require("../models/user.model")
+
+async function registerUser(req,res){
+
+    const {username,email,password}=req.body
+
+    const isAlreadyRegistered=await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    })
+
+    if(isAlreadyRegistered)
+    {
+        return res.status(400).json({
+            message:"User Already Exist"
+        })
+    }
+
+    const hash=await bcrypt.hash(password,10)
+
+    const user=await userModel.create({
+        username,
+        email,
+        password:hash
+    })
+
+    const token=jwt.sign({
+        id:user_id,
+        username:user.username
+    },process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.cookie("token",token)
+
+    return res.status(201).json({
+        message:'User Reistered Succesfully'
+    })
+
+}
+
+async function loginUser(req,res){
+    const {email,password,password}=req.body
+
+    const user=await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    })
+    if(!user){
+        return res.status(400).json({
+            message:"invalid Credentials"
+        })
+    }
+
+    const isPasswordValid=await bcrypt.compare(password,user.password)
+
+    if(!isPasswordValid)
+    {
+        return res.status(400).json({
+            message:"Invalid Credentials"
+        })
+    }
+
+    const token=jwt.sign({
+        id:user._id,
+        username:user.username
+    },process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.cookie("token",token)
+
+    return res.status(200).json({
+        message:"User LoggedIn Succesfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+}
+
+
+
+module.exports={registerUser,loginUser}
